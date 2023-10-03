8ada08c48cce1654d5f3ac2ef47f552325adc7e6
Eduardo Meireles <126266667+EduardoMeireles390@users.noreply.github.com>
2023-10-03 16:09:40 -0300

    atualização front

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,3 @@
-if (localStorage.getItem("token") == null) {
-  alert("Você precisa estar logado para acessar essa página");
-  window.location.href = "./singup.html";
-}
-
-const userLogado = JSON.parse(localStorage.getItem("userLogado"));
-
-const logado = document.querySelector("#logado");
-logado.innerHTML = `Olá ${userLogado.nome}`;
-
-function sair() {
-  localStorage.removeItem("token");
-  localStorage.removeItem("userLogado");
-  window.location.href = "./assets/html/signin.html";
-}
-
 function showLoginDialog() {
     document.getElementById("login-dialog").show();
   }
